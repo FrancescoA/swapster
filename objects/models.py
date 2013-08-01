45b585de54f9bc91ba07a8e9af0002481585c7e5
Franesco Agosti <fra5@duke.edu>
2013-08-01 00:47:28 -0400

    Basta

--- objects/models.py
+++ objects/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 
 class Object(models.Model):
+	owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="object")
 	name = models.CharField(max_length=30)
+	summary = models.CharField(max_length=500)
+	description = models.TextField(blank=True)
+	image = models.ImageField(upload_to="objects/")
 	date_created = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return self.name
--- swapster/views.py
+++ swapster/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 from django.shortcuts import render,get_object_or_404
 from trader.models import Trader
+from objects.models import Object
 
 def index(request):
+	print Object.objects.all()
+	print request.user.object_set.all()
 	return render(request, 'swapster/profile.html')
