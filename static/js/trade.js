1dbb42400c67c18f93ecdbe5d25c42714978acbb
Franesco Agosti <fra5@duke.edu>
2013-08-04 16:46:38 -0400

    More progress

--- static/js/trade.js
+++ static/js/trade.js
@@ -2,14 +2,21 @@
 
 $(function(){
 
+	/*
 	var popoptions = {
 		trigger : 'hover',
-		title : "HELO"
+		title : "More Detail",
+		placement: 'top',
+		delay : { show: 500, hide: 400 },
 	}
 
 	$('.draggable').popover(popoptions)
+	
+	*/
 
 	var dragoptions = {
+		snap : ".box",
+		zIndex: 100,
 	}
 
 	$(".draggable").draggable(dragoptions)
