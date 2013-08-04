8510751736370d595f15bd2a620c9096aa81cb61
Franesco Agosti <fra5@duke.edu>
2013-08-04 17:12:59 -0400

    Bug

--- static/js/trade.js
+++ static/js/trade.js
@@ -14,14 +14,20 @@ $(function(){
 	
 	*/
 
-	var dragoptions = {
+	var dragoptions1 = {
 		snap : ".snap1",
 		zIndex: 5,
 		snapMode: "inner",
 	}
 
-	$(".draggable1").draggable(dragoptions)
+	$(".draggable1").draggable(dragoptions1)
 
+	var dragoptions2 = {
+		snap : ".snap2",
+		zIndex: 5,
+		snapMode: "inner",
+	}
 
+	$(".draggable2").draggable(dragoptions2)
 
 });
