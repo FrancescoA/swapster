71789ff151e23bed42cf304d29384d3822ae3c36
Franesco Agosti <fra5@duke.edu>
2013-08-04 17:22:58 -0400

    Using template system for snap elements

--- static/js/trade.js
+++ static/js/trade.js
@@ -13,7 +13,6 @@ $(function(){
 	$('.draggable').popover(popoptions)
 	
 	*/
-
 	var dragoptions1 = {
 		snap : ".snap1",
 		zIndex: 5,
@@ -26,8 +25,35 @@ $(function(){
 		snap : ".snap2",
 		zIndex: 5,
 		snapMode: "inner",
+		stop: snapTrack,
 	}
 
 	$(".draggable2").draggable(dragoptions2)
+
+	var snapmap = {}
+	function snapTrack(){
+
+		/* Get the possible snap targets: */
+		var snapped = $(this).data('ui-draggable').snapElements;
+
+					/* Pull out only the snap targets that are "snapping": */
+		var snappedTo = $.map(snapped, function(element) {
+			return element.snapping ? element.item : null;
+		});
+		//Basically snappedTo is the span element that this word "snappedTo"
+					
+		//Every time map:
+		// the id of the blank we snapped to (which is equal to the correct word) 
+		 //     to
+		// the inner html of "this" (the word). To check for correctness we just make sure that key == value
+		for(var i = 0 ; i < snappedTo.length ; i++){
+			snapmap[snappedTo[i].id] = this.innerHTML;
+		}
+
+
+	}
+				
+
+
 
 });
