7cf4a16a7b581e96775ba2680bee6b0544ef4046
Franesco Agosti <fra5@duke.edu>
2013-08-05 13:16:51 -0400

    Masonry working

---
+++ static/js/objects.js
@@ -0,0 +1,14 @@
+
+
+$(function() {
+
+	var $container = $('#object-content');
+// initialize
+	$container.masonry({
+  		//columnWidth: 220,
+  		gutter : 0,
+  		isFitWidth : true,
+  		itemSelector: '.col-3'
+	});
+
+});
